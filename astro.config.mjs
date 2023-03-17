62a9e40b7aef8aec3497eb31f01327aac19eb344
kader1303 <kdryere1314@gmail.com>
2023-03-17 18:51:16 -0400

    Support my work added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://kadergomez.dev',
   integrations: [mdx(), sitemap(), tailwind()]
 });
