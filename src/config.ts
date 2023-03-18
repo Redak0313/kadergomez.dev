a9d4bc926144bce18f986061e9c9abdd3cd2367b
kader1303 <kdryere1314@gmail.com>
2023-03-18 11:08:58 -0400

    upadated

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,4 @@
 
 export const SITE_TITLE = 'Kader Gómez Céspedes | Portfolio';
 // site description in English
-export const SITE_DESCRIPTION = 'My name is Kader Gómez, I am a Fuul Stack Developer and Civil Engineer. My passion is to never stop learnning and creating solutions.';
-// export const SITE_DESCRIPTION = 'Mi nombre es Kader Gómez soy un Full Stack Dev e Ingeniero Civil. Mi pasión es nunca dejar de aprender y crear nuevas soluciones y proyectos cada día.';
+export const SITE_DESCRIPTION = 'My name is Kader Gómez, I am a Full Stack Developer and Civil Engineer. My passion is to never stop learnning and creating solutions.';
